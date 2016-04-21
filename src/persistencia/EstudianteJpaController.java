513b874678addb5fada74d38b444af5845bb3fb4
jsnar <jsnar@DESKTOP-QTBGPJL>
2016-04-20 21:47:43 -0500

    Subir proyecto al repositorio

---
+++ src/persistencia/EstudianteJpaController.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.Estudiante;
+import persistencia.exceptions.IllegalOrphanException;
+import persistencia.exceptions.NonexistentEntityException;
+import persistencia.exceptions.PreexistingEntityException;
+
+/**
+ *
+ * @author jsnar
+ */
+public class EstudianteJpaController implements Serializable {
+
+    public EstudianteJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+    public EstudianteJpaController() {
+        this.emf = Persistence.createEntityManagerFactory("TallerIHCPU");
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Estudiante estudiante) throws PreexistingEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(estudiante);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (findEstudiante(estudiante.getDocumentoestudiante()) != null) {
+                throw new PreexistingEntityException("Estudiante " + estudiante + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Estudiante> findEstudianteEntities() {
+        return findEstudianteEntities(true, -1, -1);
+    }
+
+    public List<Estudiante> findEstudianteEntities(int maxResults, int firstResult) {
+        return findEstudianteEntities(false, maxResults, firstResult);
+    }
+
+    private List<Estudiante> findEstudianteEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Estudiante.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Estudiante findEstudiante(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Estudiante.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getEstudianteCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Estudiante> rt = cq.from(Estudiante.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
